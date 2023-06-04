532b350b0e4819406a76a297e5014e34caf4824a
danielezertau <danielezer@mail.tau.ac.il>
2023-06-04 19:28:10 +0300

    DQN initial commit

---
+++ dqn/plots.py
@@ -0,0 +1,18 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    with open("statistics.pkl", 'rb') as f:
+        stats = pickle.load(f)
+    plt.title("DQN Basic Results")
+    plt.xlabel('Num Timesteps')
+    plt.ylabel('Rewards')
+    plt.plot(stats['mean_episode_rewards'], label="Mean 100-Episode Reward")
+    plt.plot(stats['best_mean_episode_rewards'], label="Best Mean Reward")
+    max_reward = np.max(stats['best_mean_episode_rewards'])
+    plt.axhline(y=max_reward, color='g', linestyle='dashed')
+    plt.yticks(list(plt.yticks()[0]) + [max_reward])
+    plt.legend()
+    plt.savefig("Q1.pdf")
+    plt.show()
