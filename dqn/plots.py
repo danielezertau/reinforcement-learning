71acd47789043bed720f6a2c00dac5f865ba8983
danielezertau <danielezer@mail.tau.ac.il>
2023-06-12 00:19:03 +0300

    Add code for plotting the DQN results with different hyper-params

--- dqn/plots.py
+++ dqn/plots.py
@@ -1,10 +1,10 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-
 import dqn_learn
 
-if __name__ == '__main__':
+
+def q1():
     with open(dqn_learn.STATS_FILE, 'rb') as f:
         stats = pickle.load(f)
     plt.title("DQN Basic Results")
@@ -14,9 +14,28 @@ if __name__ == '__main__':
     plt.plot(stats['best_mean_episode_rewards'][:6000000], label="Best Mean Reward")
     max_reward = np.max(stats['best_mean_episode_rewards'])
     plt.axhline(y=max_reward, color='g', linestyle='dashed')
-    yticks = list(plt.yticks()[0])
-    yticks.remove(20.00)
-    plt.yticks(yticks + [max_reward])
     plt.legend()
     plt.savefig("Q1.pdf")
     plt.show()
+
+
+def q2(files, labels, fig_name):
+    plt.title("DQN Hyper-Parameters Tuning")
+    plt.xlabel('Num Timesteps')
+    plt.ylabel('Rewards')
+    for file, label in zip(files, labels):
+        with open(f"{dqn_learn.STATS_DIR}/{file}", 'rb') as f:
+            stats = pickle.load(f)
+            plt.plot(stats['best_mean_episode_rewards'][:6000000], label=label)
+    plt.legend()
+    plt.savefig(fig_name)
+    plt.show()
+
+
+if __name__ == '__main__':
+    q2(files=["statistics-exploration-01.pkl", "statistics-exploration-001.pkl", "statistics-orig.pkl"],
+       labels=["final=0.01", "final=0.001", "original"], fig_name="Q2-final-p.pdf")
+    q2(files=["statistics-exploration-01-25M.pkl", "statistics-exploration-25.pkl",
+              "statistics-exploration-001-25M.pkl", "statistics-orig.pkl"],
+       labels=["final=0.01, num_steps=2.5M", "num_steps=2.5M", "final=0.001, num_steps=2.5M", "original"],
+       fig_name="Q2-num-steps.pdf")
