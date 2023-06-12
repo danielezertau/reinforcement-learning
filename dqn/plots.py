1ef6d182a269eea5132d286f4ecfc240c8ea4536
danielezertau <danielezer@mail.tau.ac.il>
2023-06-12 23:03:04 +0300

    Add the multiplicative schedule to the plot and annotate the maximal value

--- dqn/plots.py
+++ dqn/plots.py
@@ -23,18 +23,30 @@ def q2(files, labels, fig_name):
     plt.title("DQN Hyper-Parameters Tuning")
     plt.xlabel('Num Timesteps')
     plt.ylabel('Rewards')
+    max_reward = 0
     for file, label in zip(files, labels):
         with open(f"{dqn_learn.STATS_DIR}/{file}", 'rb') as f:
             stats = pickle.load(f)
+            max_reward = max(max_reward, np.max(stats['best_mean_episode_rewards']))
             plt.plot(stats['best_mean_episode_rewards'][:6000000], label=label)
+    plt.axhline(y=max_reward, color='g', linestyle='dashed')
+    yticks = list(plt.yticks()[0])
+    yticks.remove(20.00)
+    plt.yticks(yticks + [max_reward])
     plt.legend()
     plt.savefig(fig_name)
     plt.show()
 
 
+def annotate_max(stats):
+    max_reward = np.max(stats['best_mean_episode_rewards'])
+    plt.axhline(y=max_reward, color='g', linestyle='dashed')
+
+
 if __name__ == '__main__':
-    final_p_files = ["statistics-exploration-01.pkl", "statistics-exploration-001.pkl", "statistics-orig.pkl"]
-    final_p_labels = ["final=0.01", "final=0.001", "original"]
+    final_p_files = ["statistics-exploration-01.pkl", "statistics-exploration-001.pkl",
+                     "statistics-exploration-multiplicative-schedule.pkl", "statistics-orig.pkl"]
+    final_p_labels = ["final=0.01", "final=0.001", "multiplicative schedule", "original"]
     q2(files=final_p_files, labels=final_p_labels, fig_name="Q2-final-p.pdf")
     plt.close()
     num_steps_files = ["statistics-exploration-01-25M.pkl", "statistics-exploration-25.pkl",
